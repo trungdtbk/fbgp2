fcc7ad78866546f59de3411a44daa56c7abb878a
Trung Truong <trungdtbk@gmail.com>
2019-02-22 04:23:41 +1300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name = 'fbgp',
+    version = '1.0.0',
+    author = 'Trung Truong',
+    author_email = 'trungdtbk@gmail.com',
+    description = ("A software defined BGP router."),
+    license = 'BSD',
+    long_description = read('README.rst'),
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Networking :: Routing :: BGP",
+        "Programming Language :: Python",
+        "License :: BSD License",
+        ],
+    packages=find_packages(exclude=['tests']),
+    entry_points = {
+        'console_scripts': [
+            'fbgp_exabgp_hook=fbgp.exabgp_hook:main',
+            ]
+        },
+    install_requires=[
+            'pyyaml',
+            'ryu',
+            'oslo.config'
+            ]
+    )
